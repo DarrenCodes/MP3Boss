6f3ed0d7e7d8b3dade9a9a1b4865772bfd621ec3
Darren Johnson <darrenthenoobie@gmail.com>
2015-11-09 00:15:00 +0200

    Minor changes.

--- MP3Boss/Main/Interfaces/IFileManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-namespace MP3Boss
-{
-    public interface IFileManager
-    {
-        List<string> getAudioFiles(string[] dropedFiles);
-        bool renameAudioFile(string originalFile, string newFileName);
-    }
-}
--- MP3Boss/Main/Interfaces/ISave.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-namespace MP3Boss
-{
-    public interface ISave
-    {
-        void saveManager(bool applyToAll, bool autoNext, int format, IMainForm iMainForm);
-        void searchAndReplace(string find, string replace, List<string> mp3Files, int currentIndex, bool searchAll);
-    }
-}
