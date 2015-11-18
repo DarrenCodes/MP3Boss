379888e93bee61e7b7e35654a600e18f1f1f10f8
Darren Johnson <darrenthenoobie@gmail.com>
2015-11-18 11:03:19 +0200

    Added a feature that allows one or all loaded files' tags to be entered into the database and used when looking for suggestions

---
+++ MP3Boss/Source/Database/IDatabaseAdd.cs
@@ -0,0 +1,9 @@
+﻿using MP3Boss.Source.Datastructures;
+
+namespace MP3Boss.Source.Database
+{
+    public interface IDatabaseAdd
+    {
+        bool AddToDatabase(IFormComboBoxContainer valueToAdd);
+    }
+}
